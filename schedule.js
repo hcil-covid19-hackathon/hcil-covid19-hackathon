915655feb8a138a11bc4a94f45c1450353011865
Andrea Batch <andrea.julca@gmail.com>
2020-03-30 23:34:17 -0400

    ready to stage

---
+++ schedule.js
@@ -0,0 +1,31 @@
+var scheduleShown = false;
+$(document).ready( function() {            
+    $("#load_schedule").on("click", function() {
+        if(!scheduleShown){
+            $("#schedule").load("schedule.html", function( response, status, xhr ) {
+                var coll = document.getElementsByClassName("collapsible");
+                var i;
+
+                for (i = 0; i < coll.length; i++) {
+                coll[i].addEventListener("click", function() {
+                    this.classList.toggle("active");
+                    var content = this.nextElementSibling;
+                    if (content.style.maxHeight){
+                    content.style.maxHeight = null;
+                    } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                    } 
+                });
+                }
+            });
+
+            scheduleShown = true;
+        } else {
+            var node = document.getElementById("schedule");
+            while (node.hasChildNodes()) {
+                node.removeChild(node.lastChild);
+            }
+            scheduleShown = false;
+        }
+    });
+});
